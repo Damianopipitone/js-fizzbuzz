c46cb21d2d6dce11d9d8ffa630a9552fb061f1d5
Damianopipitone <damianopipitone33@gmail.com>
2023-03-30 16:35:12 +0200

    FizzBuzz

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 === 0) {
         console.log("FizzBuzz");
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square">
+            <p>${"FizzBuzz"}</p>
+        </div>
+        `;
     } else if (i % 3 === 0) {
         console.log("Fizz");
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square">
+            <p>${"Fizz"}</p>
+        </div>
+        `;
     } else if (i % 5 === 0) {
         console.log("Buzz");
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square">
+            <p>${"Buzz"}</p>
+        </div>
+        `;
     } else {
         console.log(i);
     };
+    document.getElementById("container-for-squares").innerHTML += `
+    <div class="square">
+        <p>${i}</p>
+    </div>
+    `;
 };
 
